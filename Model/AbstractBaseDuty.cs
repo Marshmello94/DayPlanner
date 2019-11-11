bdc1ca9bfe9088d7f05f156223c0f4c7d6acdcec
Kirill Pavlov <ksmersh@gmail.com>
2019-11-11 11:40:52 +0300

    Новая ветвь. Добавлен класс - AbstractBaseDuty

---
+++ Model/AbstractBaseDuty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egednevnik.Model
+{
+    abstract class AbstractBaseDuty
+    {
+        private protected string _title;
+        private protected string _description;
+        private protected AbstractBaseDuty(string Title,string Description)
+        {
+            _title = Title;
+            _description = Description;
+        }
+    }
+}
